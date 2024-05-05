580abf2685e51cfce3681aebff9ff8d8acaec979
abc <abc@example.com>
2024-05-05 12:25:54 +0530

    test

--- src/main/java/com/mart/radhakrishnamart/To/Product.java
+++ src/main/java/com/mart/radhakrishnamart/To/Product.java
@@ -1,9 +1,5 @@
 package com.mart.radhakrishnamart.To;
 
-
-
-
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -21,50 +17,43 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
-
-
 @Entity
-@Table(name = "proudcts", uniqueConstraints={@UniqueConstraint(columnNames = {"name"})})
+@Table(name = "proudcts", uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
 
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	
+
 	private String name;
 
 	private double price;
-	
+
 	private String discription;
-	
+
 	@ElementCollection
 	private List<String> images;
-	
-	@ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "category_id")
-	 @JsonIgnoreProperties("products")
-    private Category category;
 
-	
-	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "category_id")
+	@JsonIgnoreProperties("products")
+	private Category category;
+
 	public Product() {
-		
+
 	}
 
-	
-	public Product(long id, String name,  double price, String discription, Category category, List<String> images) {
+	public Product(long id, String name, double price, String discription, Category category, List<String> images) {
 		super();
 		this.id = id;
 		this.name = name;
-		
+
 		this.price = price;
 		this.discription = discription;
 		this.category = category;
 		this.images = images;
 	}
-
-
 
 	public long getId() {
 		return id;
@@ -110,15 +99,8 @@ public class Product {
 		return images;
 	}
 
-
 	public void setImages(List<String> images) {
 		this.images = images;
 	}
 
-
-
-		
-	}
-	
-
-
+}
